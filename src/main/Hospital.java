10cf9fa143e0c5e070e3d9d5dd8a50a07c986354
darshan-tv <darshans@testvagrant.com>
2021-06-22 17:46:50 +0530

    Added unit tests

--- src/main/Hospital.java
+++ src/main/Hospital.java
@@ -1,32 +1,17 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Hospital {
 
+    List<Patient> patientList = new ArrayList<>();
+    public void addPatient(Patient... patient) {
+        patientList.addAll(Arrays.asList(patient));
+    }
 
     public List<Patient> getPatients() {
-        List<Patient> patientList = new ArrayList<>();
-
-        LocalDate currentDate = LocalDate.now();
-        LocalDate currentDateMinus3Days = currentDate.minusDays(3);
-        LocalDate currentDateMinus5Days = currentDate.minusDays(5);
-
-        Patient p1 = new OutPatient("Patient1", "Bengaluru", currentDate);
-        Patient p2 = new OutPatient("Patient2", "Bengaluru", currentDate);
-        Patient p3 = new OutPatient("Patient3", "Mysuru", currentDate);
-        Patient p4 = new OutPatient("Patient4", "Bengaluru", currentDateMinus5Days);
-        Patient p5 = new OutPatient("Patient5", "Chikmagalore", currentDateMinus5Days);
-        Patient p6 = new OutPatient("Patient6", "Kolar", currentDateMinus3Days);
-
-        patientList.add(p1);
-        patientList.add(p2);
-        patientList.add(p3);
-        patientList.add(p4);
-        patientList.add(p5);
-        patientList.add(p6);
-
         return patientList;
     }
 
@@ -41,7 +26,7 @@ public class Hospital {
     public List<Patient> getPatientsOutOfBengaluru(LocalDate startDate, LocalDate endDate){
         List<Patient> patients = getPatients();
         return patients.stream().parallel().filter(patient -> {
-            return "Bengaluru".equalsIgnoreCase(patient.getPatientLoc()) && patient.getPatientRegTime().isAfter(startDate)
+            return !"Bengaluru".equalsIgnoreCase(patient.getPatientLoc()) && patient.getPatientRegTime().isAfter(startDate)
                     && patient.getPatientRegTime().isBefore(endDate);
         }).collect(Collectors.toList());
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,23 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args){
+        Hospital hospital = new Hospital();
+
+        LocalDate currentDate = LocalDate.now();
+        LocalDate currentDateMinus3Days = currentDate.minusDays(3);
+        LocalDate currentDateMinus5Days = currentDate.minusDays(5);
+
+        Patient p1 = new OutPatient("Patient1", "Bengaluru", currentDate);
+        Patient p2 = new OutPatient("Patient2", "Bengaluru", currentDate);
+        Patient p3 = new OutPatient("Patient3", "Mysuru", currentDate);
+        Patient p4 = new OutPatient("Patient4", "Bengaluru", currentDateMinus5Days);
+        Patient p5 = new OutPatient("Patient5", "Chikmagalore", currentDateMinus5Days);
+        Patient p6 = new OutPatient("Patient6", "Kolar", currentDateMinus3Days);
+        hospital.addPatient(p1,p2,p3,p4,p5,p6);
+
         LocalDate startDate = LocalDate.of(2021, 06, 14);
         LocalDate endDate = LocalDate.of(2021, 06, 23);
-        Hospital hospital = new Hospital();
+
         int noOfPatientsBengaluru = hospital.getPatientsFromBengaluru(startDate, endDate).size();
         int noOfPatientsOutOfBengaluru = hospital.getPatientsOutOfBengaluru(startDate, endDate).size();
         int totalNoOfPatients = hospital.getNoOfPatients(startDate, endDate).size();
